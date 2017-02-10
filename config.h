c71c7eff2cb06189ce095c9156c1b11d6c0e45d9
LawrenceHan <yangfei6565@163.com>
2017-02-10 18:17:15 +0800

    added new files.

---
+++ config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+//  需要配合服务器设置的
+#define SETUP_API_ID(apiId) apiId = 74853;
+#define SETUP_API_HASH(apiHash) apiHash = @"be5c09cd1f599a5c0a813b8f45f0df8e";
+//  @albtsantos https://github.com/peter-iakovlev/Telegram/issues/34
+
+#endif
